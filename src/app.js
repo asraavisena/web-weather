954d53bcf838059a2a191809b3f55ed14a589535
asraavisena <asra.avisena@yahoo.com>
2019-08-30 21:02:14 +0700

    add about text and some few changes

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ app.use(express.static(publicDirPath))
 
 app.get('', (req,res)=>{
     res.render('index',{
-        title:'Trying dynamic page with hbs',
+        title:'My Weather App',
         name: 'Asra Avisena'
     })
 })
